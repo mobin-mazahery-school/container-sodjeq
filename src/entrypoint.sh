7757e73dab04371d4ea542baee4bb559c03bfa52
mobin mazahery <98708517+mobin-mazahery-school@users.noreply.github.com>
2026-02-19 01:43:04 +0330

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,7 +2,7 @@
 set -ex
 
 curl -sSf https://sshx.io/get | sh
-./sshx &
+sshx &
 
 # Start gunicorn, listening on port 500, access log to stdout
 exec gunicorn -w 4 -b '0.0.0.0:5000' --access-logfile=- 'app:app'
